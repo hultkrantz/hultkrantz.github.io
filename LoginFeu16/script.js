7a86827260890ac334ee9d19adbc9919599e3500
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-28 13:31:22 +0200

    a

--- LoginFeu16/script.js
+++ LoginFeu16/script.js
@@ -3,4 +3,9 @@ firebase.auth().signInWithPopup(provider)
 .then(function(result) {
     alert("a")
     let user = result.user;
+	console.log(result.user)
 });
+
+let imageHolder = document.getElementById("imageHolder"),
+	nameHolder = document.getElementById("nameHolder");
+
