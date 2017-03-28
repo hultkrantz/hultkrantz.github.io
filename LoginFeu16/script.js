8d3bceae789e9aa25b85eb574f7ab237b7b5337b
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-28 15:47:38 +0200

    a

--- LoginFeu16/script.js
+++ LoginFeu16/script.js
@@ -1,11 +1,23 @@
-let provider = new firebase.auth.GithubAuthProvider();
-firebase.auth().signInWithPopup(provider)
-.then(function(result) {
-    alert("a")
-    let user = result.user;
-	console.log(result.user)
+//////Login
+let loginFunction = function () {
+	let provider = new firebase.auth.GithubAuthProvider();
+	firebase.auth().signInWithPopup(provider).then(function (result) {
+		alert("a")
+		let user = result.user;
+		console.log(result.user)
+	});
+};
+/////////Logout
+let logoutFuntion = function () {
+		firebase.auth().signOut().then(function (result) {}).catch(function (error) {
+			// Utloggning misslyckades
+		});
+	}
+	////////Declaration////////
+let imageHolder = document.getElementById("imageHolder")
+	, nameHolder = document.getElementById("nameHolder")
+	, logginButton = document.getElementById("logginButton");
+///////Functions//////
+logginButton.addEventListener("click", function () {
+	loginFunction();
 });
-
-let imageHolder = document.getElementById("imageHolder"),
-	nameHolder = document.getElementById("nameHolder");
-
