bfd508b1307735dbb934d5de136b598b2a1b4cb6
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-29 13:41:14 +0200

    ab

--- LoginFeu16/script.js
+++ LoginFeu16/script.js
@@ -26,7 +26,7 @@ let logoutFuntion = function () {
 let displayProfileImage = function () {
     let profileImage = document.createElement("img");
     console.log("Profile image: "+profileImage);
-    profileImage.src = localStorage.getItem(user.photoURL);
+    profileImage.src = localStorage.getItem(user.photoURL.value);
     imageHolder.appendChild(profileImage);
     console.log("Profile image: "+profileImage);
 };
