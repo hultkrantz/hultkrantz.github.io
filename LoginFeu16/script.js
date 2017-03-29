77a2484a0ad1482f10864173bf5e888329f0c3eb
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-29 11:45:58 +0200

    a

--- LoginFeu16/script.js
+++ LoginFeu16/script.js
@@ -1,37 +1,33 @@
 ////////Declaration////////
 let imageHolder = document.getElementById("imageHolder")
-	, nameHolder = document.getElementById("nameHolder")
-	, loginButton = document.getElementById("loginButton");
-
+    , nameHolder = document.getElementById("nameHolder")
+    , loginButton = document.getElementById("loginButton");
 //////Login
 let loginFunction = function () {
-	let provider = new firebase.auth.GithubAuthProvider();
-	firebase.auth().signInWithPopup(provider).then(function (result) {
-		alert("ab")
-		let user = result.user;
-		console.log(result.user)
-	});
+    let provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        alert("ab");
+        let user = result.user;
+        console.log(result.user);
+    });
 };
 /////////Logout
 let logoutFuntion = function () {
-		firebase.auth().signOut().then(function (result) {
-			console.log("Du är nu utloggad");
-		}).catch(function (error) {
-			// Utloggning misslyckades
-			console.log("Det gick inte som vi ville" + error)
-		});
-	}
-	
-///////Functions//////
-
-if (loginButton.innerHTML === "Sign out") {
-	loginButton.addEventListener("click", function () {
-		logoutFuntion();
-		loginButton.innerHTML = "Sign in";
-	})
-}else{
-	loginButton.addEventListener("click", function () {
-	loginFunction();
-	loginButton.innerHTML = "Sign out";
+        firebase.auth().signOut().then(function (result) {
+            console.log("Du är nu utloggad");
+        }).catch(function (error) {
+            // Utloggning misslyckades
+            console.log("Det gick inte som vi ville" + error);
+        });
+    };
+    ///////Functions//////
+loginButton.addEventListener("click", function () {
+    if (loginButton.innerHTML === "Sign out") {
+        logoutFuntion();
+        loginButton.innerHTML = "Sign in";
+    }
+    else {
+            loginFunction();
+            loginButton.innerHTML = "Sign out";
+    }
 });
-}
