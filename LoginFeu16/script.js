96c3fa97806e99bf714b2cb6e223aebfeded47f6
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-30 09:43:37 +0200

    a

--- LoginFeu16/script.js
+++ LoginFeu16/script.js
@@ -8,7 +8,7 @@ let imageHolder = document.getElementById("imageHolder")
 	, user, userProfileUrl, profileImage;
 /////////Secret User
 let secretFunction = function () {
-	if (user.email === carl.hultkrantz@gmail.com) {
+	if (user.email === "carl.hultkrantz@gmail.com") {
 		secretButton.style.visibility = "visible";
 	}
 };
