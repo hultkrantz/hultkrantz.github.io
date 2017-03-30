f6744f1b142dc37d0c2ceb4e86b0f65999074965
hultkrantz <carl.hultkrantz@gmail.com>
2017-03-30 09:23:40 +0200

    a

--- LoginFeu16/script.js
+++ LoginFeu16/script.js
@@ -1,61 +1,63 @@
 ////////Declaration////////
 let imageHolder = document.getElementById("imageHolder")
-    , nameHolder = document.getElementById("nameHolder")
-    , loginButton = document.getElementById("loginButton")
-    , logoutButton = document.getElementById("logoutButton")
-    , pictureButton = document.getElementById("pictureButton")
-    , imageMessage = document.getElementById("imageMessage")
-    , user, userProfileUrl, profileImage;
+	, nameHolder = document.getElementById("nameHolder")
+	, loginButton = document.getElementById("loginButton")
+	, logoutButton = document.getElementById("logoutButton")
+	, secretButton = document.getElementById("secretButton")
+	, imageMessage = document.getElementById("imageMessage")
+	, user, userProfileUrl, profileImage;
 //////Login
 let loginFunction = function () {
-    let provider = new firebase.auth.GithubAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        user = result.user;
-        userProfileUrl = user.photoURL;
-        displayProfileImage();
-        if (user.displayName === null) {
-            nameHolder.innerHTML = "Welcome " + user.email+"!";
-        }
-        else if (user.displayName === undefined){
-            console.log("Detta fungerar ju inte.");
-        } else {
-			nameHolder.innerHTML = "Welcome " + user.displayName+"!";
-			
+	let provider = new firebase.auth.GithubAuthProvider();
+	firebase.auth().signInWithPopup(provider).then(function (result) {
+		user = result.user;
+		userProfileUrl = user.photoURL;
+		displayProfileImage();
+		if (user.displayName !== null) {
+			nameHolder.innerHTML = "Welcome " + user.displayName + "!";
 		}
-    });
+		else if (firebase.auth().currentUser.providerData[0].displayName) {
+			nameHolder.innerHTML = "Welcome " + firebase.auth().currentUser.providerData[0].displayName+"!";
+		}
+		else {
+			nameHolder.innerHTML = "Welcome " + user.email + "!";
+		}
+	});
+	secretFunction();
 };
 /////////Logout
 let logoutFuntion = function () {
-    firebase.auth().signOut().then(function (result) {
-        console.log("You are no more my friend");
-    }).catch(function (error) {
-        // Utloggning misslyckades
-        console.log("Det gick inte som vi ville" + error);
-        nameHolder.innerHTML = "Somting went wrong";
-        
-    });
+	firebase.auth().signOut().then(function (result) {
+		console.log("You are no more my friend");
+	}).catch(function (error) {
+		// Utloggning misslyckades
+		console.log("Det gick inte som vi ville" + error);
+		nameHolder.innerHTML = "Somting went wrong";
+	});
 };
 ///////Functions//////
 let displayProfileImage = function () {
-    imageHolder.innerHTML = "";
-    profileImage = document.createElement("img");
-    profileImage.src = userProfileUrl;
-    profileImage.style.maxHeight ="100%";
-    profileImage.style.maxWidth ="100%";
-    imageHolder.appendChild(profileImage);
+	imageHolder.innerHTML = "";
+	profileImage = document.createElement("img");
+	profileImage.src = userProfileUrl;
+	profileImage.style.maxHeight = "100%";
+	profileImage.style.maxWidth = "100%";
+	imageHolder.appendChild(profileImage);
 };
 loginButton.addEventListener("click", function () {
-    loginFunction();
-    console.log("loginButton pressed");
-    loginButton.style.display = "none";
-    logoutButton.style.display = "inline";
-
+	loginFunction();
+	console.log("loginButton pressed");
+	loginButton.style.display = "none";
+	logoutButton.style.display = "inline";
 });
 logoutButton.addEventListener("click", function () {
-    logoutFuntion();
-    console.log("logoutButton pressed");
-    nameHolder.innerHTML = "Another time then";
-    loginButton.style.display = "inline";
-    logoutButton.style.display = "none";
-    profileImage.src ="";
+	logoutFuntion();
+	console.log("logoutButton pressed");
+	nameHolder.innerHTML = "Another time then";
+	loginButton.style.display = "inline";
+	logoutButton.style.display = "none";
+	profileImage.src = "";
+});
+secretButton.addEventListener("click", function () {
+	alert("The cake is a lie");
 });
