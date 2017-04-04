aed7d7b049b9ac79424b430420ebdda178f120e3
hultkrantz <carl.hultkrantz@gmail.com>
2017-04-04 13:19:10 +0100

    aa

--- sortFeu16/script.js
+++ sortFeu16/script.js
@@ -1,23 +1,29 @@
 window.addEventListener('load', function () {
 	let inputItemName = document.getElementById("inputItemName")
-		, inputSize = document.getElementById("inputSize")
+		, inputQuantity = document.getElementById("inputQuantity")
 		, inputPrice = document.getElementById("inputPrice")
 		, addItemButton = document.getElementById('addItemButton')
 		, itemTable = document.getElementById('itemTable')
 		, alfabeticSort = document.getElementById('alfabeticSort')
-		, sizeSort = document.getElementById('sizeSort')
+		, quantitySort = document.getElementById('quantitySort')
 		, priceLowToHigh = document.getElementById('priceLowToHigh')
 		, priceHighToLow = document.getElementById('priceHighToLow')
-		, numberOfViews = document.getElementById("numberOfViews");
+		, numberOfViews = document.getElementById("numberOfViews")
+		, selectSort = document.getElementById("selectSort");
 	///
 	//let inputAntalResultat = document.getElementById('inputAntalResultat');
 	addItemButton.addEventListener('click', function (event) {
 		console.log('addItemButton pressed');
+		if(inputItemName.value === "" ||inputPrice.value === "" || inputQuantity.value === ""){
+			alert("Input empty")
+		} else {
 		firebase.database().ref('stock/').push({
 			name: inputItemName.value
-			, size: inputSize.value
-			, price: inputPrice.value
+			, quantity: Number(inputQuantity.value)
+			, price: Number(inputPrice.value)
+			
 		});
+			}
 	});
 			firebase.database().ref('stock/').on('child_added', function (snapshot, keyStone) {
 				console.log('First time or change in database. keyStone: ' + keyStone);
@@ -27,14 +33,13 @@ window.addEventListener('load', function () {
 
 			function addItemToTable(data) {
 				let tr = document.createElement('tr');
-				tr.innerHTML = `<td>${data.name}</td> <td>${data.size}</td> <td>${data.price}</td> <td style="width: 50px; background-color: ${data.färg};"></td>`;
+				tr.innerHTML = `<td>${data.name}</td> <td>${data.quantity}</td> <td>${data.price}</td> <td style="width: 50px;"></td>`;
 				itemTable.appendChild(tr);
 			}
 
 			function sortFunction(button, sortKey) {
 				button.addEventListener('click', function (event) {
 					itemTable.innerHTML = '';
-					console.log("itemTable : " + itemTable)
 						//firebase.database().ref('stock/').off('value')
 					firebase.database().ref('stock/').orderByChild(sortKey).once('value', function (snapshot) {
 						snapshot.forEach(itemRef => {
@@ -43,10 +48,11 @@ window.addEventListener('load', function () {
 					});
 				})
 			}
+
 			sortFunction(alfabeticSort, 'item');
-			sortFunction(sizeSort, 'size');
+			sortFunction(quantitySort, 'quantity');
 			sortFunction(priceLowToHigh, 'price');
-			sortFunction(priceHighToLow, 'price')
+			sortFunction(priceHighToLow, 'price');
 			numberOfViews.addEventListener('keypress', function (event) {
 				if (event.keyCode == 13) {
 					let viewNumber = Number(numberOfViews.value);
