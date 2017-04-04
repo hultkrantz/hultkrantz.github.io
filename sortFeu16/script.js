e9743642571283ccab97e15e1595ed2cc2efaf3f
hultkrantz <carl.hultkrantz@gmail.com>
2017-04-04 12:31:35 +0100

    aa

--- sortFeu16/script.js
+++ sortFeu16/script.js
@@ -7,7 +7,8 @@ window.addEventListener('load', function () {
 		, alfabeticSort = document.getElementById('alfabeticSort')
 		, sizeSort = document.getElementById('sizeSort')
 		, priceLowToHigh = document.getElementById('priceLowToHigh')
-		, priceHighToLow = document.getElementById('priceHighToLow');
+		, priceHighToLow = document.getElementById('priceHighToLow')
+		, numberOfViews = document.getElementById("numberOfViews");
 	///
 	//let inputAntalResultat = document.getElementById('inputAntalResultat');
 	addItemButton.addEventListener('click', function (event) {
@@ -17,55 +18,51 @@ window.addEventListener('load', function () {
 			, size: inputSize.value
 			, price: inputPrice.value
 		});
-		console.log(name);
 	});
-	firebase.database().ref('stock/').on('child_added', function (snapshot, keyStone) {
-		firebase.database().ref('stock/').on('child_added', function (snapshot, keyStone) {
-			console.log('First time or change in database. keyStone: ' + keyStone);
-			let data = snapshot.val();
-			console.log('data:', data);
-			addItemToTable(data);
-		});
+			firebase.database().ref('stock/').on('child_added', function (snapshot, keyStone) {
+				console.log('First time or change in database. keyStone: ' + keyStone);
+				let data = snapshot.val();
+				addItemToTable(data);
+			});
 
-		function addItemToTable(data) {
-			let tr = document.createElement('tr');
-			tr.innerHTML = `<td>${data.name}</td> <td>${data.size}</td> <td>${data.price}</td> <td style="width: 50px; background-color: ${data.färg};"></td>`;
-			itemTable.appendChild(tr);
-		}
+			function addItemToTable(data) {
+				let tr = document.createElement('tr');
+				tr.innerHTML = `<td>${data.name}</td> <td>${data.size}</td> <td>${data.price}</td> <td style="width: 50px; background-color: ${data.färg};"></td>`;
+				itemTable.appendChild(tr);
+			}
 
-		function sortFunction(button, sortKey) {
-			button.addEventListener('click', function (event) {
-				itemTable.innerHTML = '';
-				//firebase.database().ref('stock/').off('value')
-				firebase.database().ref('stock/').orderByChild(sortKey).once('value', function (snapshot) {
-					//firebase.database().ref('stock/').off('value')
+			function sortFunction(button, sortKey) {
+				button.addEventListener('click', function (event) {
+					itemTable.innerHTML = '';
+					console.log("itemTable : " + itemTable)
+						//firebase.database().ref('stock/').off('value')
 					firebase.database().ref('stock/').orderByChild(sortKey).once('value', function (snapshot) {
 						snapshot.forEach(itemRef => {
 							addItemToTable(itemRef.val());
 						})
 					});
 				})
-			})
+			}
 			sortFunction(alfabeticSort, 'item');
 			sortFunction(sizeSort, 'size');
 			sortFunction(priceLowToHigh, 'price');
-			inputAntalResultat.addEventListener('keypress', function (event) {
+			sortFunction(priceHighToLow, 'price')
+			numberOfViews.addEventListener('keypress', function (event) {
 				if (event.keyCode == 13) {
-					let antal = Number(inputAntalResultat.value);
+					let viewNumber = Number(numberOfViews.value);
 					itemTable.innerHTML = '';
-					console.log('inputAntalResultat: antal=' + antal);
-					if (isNaN(antal)) {
-						// varna användaren
+					console.log('numberOfViews is ' + viewNumber);
+					if (isNaN(viewNumber)) {
+						alter("Number of views must be numbers only")
 					}
 					else {
-						firebase.database().ref('stock/').limitToFirst(antal).once('value', function (snapshot) {
+						firebase.database().ref('stock/').limitToFirst(viewNumber).once('value', function (snapshot) {
 							snapshot.forEach(itemRef => {
-								addAnimalToTable(itemRef.val());
+								addItemToTable(itemRef.val());
 							})
 						});
 					}
-				}
-			});
-		}
-	})
-})
+				
+			}
+		});
+	});
