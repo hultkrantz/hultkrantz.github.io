0aa3729378dff9bcb6fe79b47aa3f29c11eade7d
hultkrantz <carl.hultkrantz@gmail.com>
2017-04-04 10:20:22 +0100

    aa

--- sortFeu16/script.js
+++ sortFeu16/script.js
@@ -8,6 +8,7 @@ window.addEventListener('load', function () {
 				, sizeSort = document.getElementById('sizeSort')
 				, priceLowToHigh = document.getElementById('priceLowToHigh')
 				, priceHighToLow = document.getElementById('priceHighToLow');
+			///
 			//let inputAntalResultat = document.getElementById('inputAntalResultat');
 			addItemButton.addEventListener('click', function (event) {
 				console.log('addItemButton pressed');
@@ -18,52 +19,55 @@ window.addEventListener('load', function () {
 				});
 			});
 			firebase.database().ref('stock/').on('child_added', function (snapshot, keyStone) {
-						firebase.database().ref('stock/').on('child_added', function (snapshot, keyStone) {
-							console.log('First time or change in database. keyStone: ' + keyStone);
-							let data = snapshot.val();
-							console.log('data:', data);
-							addItemToTable(data);
-						});
+				firebase.database().ref('stock/').on('child_added', function (snapshot, keyStone) {
+					console.log('First time or change in database. keyStone: ' + keyStone);
+					let data = snapshot.val();
+					console.log('data:', data);
+					addItemToTable(data);
+				});
 
-						function addItemToTable(data) {
-							let tr = document.createElement('tr');
-							tr.innerHTML = `<td>${data.namn}</td> <td>${data.familj}</td> <td>${data.antal}</td> <td style="width: 50px; background-color: ${data.färg};"></td>`;
-							tableVisaDjur.appendChild(tr);
-						}
+				function addItemToTable(data) {
+					let tr = document.createElement('tr');
+					tr.innerHTML = `<td>${data.namn}</td> <td>${data.familj}</td> <td>${data.antal}</td> <td style="width: 50px; background-color: ${data.färg};"></td>`;
+					tableVisaDjur.appendChild(tr);
+				}
 
-						function sortFunction(button, sortKey) {
-							button.addEventListener('click', function (event) {
-								tableVisaDjur.innerHTML = '';
-								//firebase.database().ref('stock/').off('value')
-								firebase.database().ref('stock/').orderByChild(sortKey).once('value', function (snapshot) {
-									//firebase.database().ref('stock/').off('value')
-									firebase.database().ref('stock/').orderByChild(sortKey).once('value', function (snapshot) {
-										snapshot.forEach(animalRef => {
-											addAnimalToTable(animalRef.val());
-										})
-									});
+				function sortFunction(button, sortKey) {
+					button.addEventListener('click', function (event) {
+						tableVisaDjur.innerHTML = '';
+						//firebase.database().ref('stock/').off('value')
+						firebase.database().ref('stock/').orderByChild(sortKey).once('value', function (snapshot) {
+							//firebase.database().ref('stock/').off('value')
+							firebase.database().ref('stock/').orderByChild(sortKey).once('value', function (snapshot) {
+								snapshot.forEach(animalRef => {
+									addAnimalToTable(animalRef.val());
 								})
-							})
-							sortFunction(alfabeticSort, 'namn');
-							sortFunction(sizeSort, 'familj');
-							sortFunction(priceLowToHigh, 'antal');
-							sortFunction(priceHighToLow, 'färg');
-							inputAntalResultat.addEventListener('keypress', function (event) {
-								if (event.keyCode == 13) {
-									let antal = Number(inputAntalResultat.value);
-									tableVisaDjur.innerHTML = '';
-									console.log('inputAntalResultat: antal=' + antal);
-									if (isNaN(antal)) {
-										// varna användaren
-									}
-									else {
-										firebase.database().ref('stock/').limitToFirst(antal).once('value', function (snapshot) {
-											snapshot.forEach(animalRef => {
-												addAnimalToTable(animalRef.val());
-											})
-										});
-									}
-								}
 							});
+						})
+					})
+					sortFunction(alfabeticSort, 'namn');
+					sortFunction(sizeSort, 'familj');
+					sortFunction(priceLowToHigh, 'antal');
+					sortFunction(priceHighToLow, 'färg');
+					inputAntalResultat.addEventListener('keypress', function (event) {
+						if (event.keyCode == 13) {
+							let antal = Number(inputAntalResultat.value);
+							tableVisaDjur.innerHTML = '';
+							console.log('inputAntalResultat: antal=' + antal);
+							if (isNaN(antal)) {
+								// varna användaren
+							}
+							else {
+								firebase.database().ref('stock/').limitToFirst(antal).once('value', function (snapshot) {
+									snapshot.forEach(animalRef => {
+										addAnimalToTable(animalRef.val());
+									})
+								});
+							}
 						}
-			});
+					});
+				}
+			})
+})
+
+						
