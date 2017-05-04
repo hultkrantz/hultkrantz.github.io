306db6672fc3c0bc9197cdd4ec783ed7d16bd677
hultkrantz <carl.hultkrantz@gmail.com>
2017-05-04 23:25:50 +0200

    bind loginFunc

--- feu16mvt16-login/script.jsx
+++ feu16mvt16-login/script.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
     }
 loginFunction() {
 	let provider = new firebase.auth.FacebookAuthProvider();
-	firebase.auth().signInWithPopup(provider).then(function (result).bind(this) {
+	firebase.auth().signInWithPopup(provider).then(function (result) {
         console.log(result.user.displayName)
 		 this.setState({
             user: result.user.displayName
