1bbca5da68dc137b39f1a7e78745840b6ddd4e9d
hultkrantz <carl.hultkrantz@gmail.com>
2017-05-04 23:07:19 +0200

    bind loginFunc

--- feu16mvt16-login/script.jsx
+++ feu16mvt16-login/script.jsx
@@ -2,7 +2,7 @@ class App extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            user,
+            user:{},
             userProfileUrl:'', 
             profileImage:''
         };
