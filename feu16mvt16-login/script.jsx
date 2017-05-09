bab5a3b7303adb7af0ea0c43a214c95a30f102b7
hultkrantz <carl.hultkrantz@gmail.com>
2017-05-09 10:12:34 +0200

    bad

--- feu16mvt16-login/script.jsx
+++ feu16mvt16-login/script.jsx
@@ -2,63 +2,47 @@ class App extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            user:'dsa',
-            userProfileUrl:'', 
-            profileImage:''
+            user: null,
+            userProfileUrl:''
         };
         //bind us together forever and ever
         this.loginFunction = this.loginFunction.bind(this);
-        /*this.googleFunction = this.googleFunction.bind(this);
-        this.logoutFunction = this.logoutFunction.bind(this);
-        this.displayProfileImage = this.displayProfileImage.bind(this);*/
+        this.googleFunction = this.googleFunction.bind(this);
+        /*this.logoutFunction = this.logoutFunction.bind(this);*/
     }
 	loginFunction() {
 		console.log('this:',this)
 		let self = this;
 		let provider = new firebase.auth.FacebookAuthProvider();
 		firebase.auth().signInWithPopup(provider).then(function (result) {
-			console.log(result.user.displayName)
-			console.log(self.state.user)
 			 self.setState({
-				user: result.user;
-				console.log('User: ', user)
-			})//info
-			if (result.user.displayName !== null) {
-				nameHolder.innerHTML = "Loggedin with Facebook. " + self.user.displayName + "!";
-			}
-			else if (firebase.auth().currentUser.providerData[0].displayName) {
-				nameHolder.innerHTML = "Loggedin with Facebook. " + firebase.auth().currentUser.providerData[0].displayName + "!";
-			}
-			else {
-				nameHolder.innerHTML = "Loggedin with Facebook. " + user.email + "!";
-			}
-			userProfileUrl = firebase.auth().currentUser.providerData[0].photoURL;
-			displayProfileImage();
-			console.log(user.photoURL)
+				user: result.user,
+				 userProfileUrl : firebase.auth().currentUser.providerData[0].photoURL
+			})
+			//// end checker
+			console.log('Email: ' , self.state.user.email)
+			console.log('DisplayName: ', self.state.user)
+			console.log('userProfilePic', self.state.userProfileUrl )
 		}).catch();
 	};
-/*
-
 googleFunction() {
-result.user
+let self = this;
 	let googleProvider = new firebase.auth.GoogleAuthProvider();
 	firebase.auth().signInWithPopup(googleProvider).then(function (result) {
-		user = result.user;
-		if (user.displayName !== null) {
-			nameHolder.innerHTML = "Loggedin with Google. " + user.displayName + "!";
-		}
-		else if (firebase.auth().currentUser.providerData[0].displayName) {
-			nameHolder.innerHTML = "Loggedin with Google. " + firebase.auth().currentUser.providerData[0].displayName + "!";
-		}
-		else {
-			nameHolder.innerHTML = "Loggedin with Google. " + user.email + "!";
-		}
-		userProfileUrl = firebase.auth().currentUser.providerData[0].photoURL;
-		displayProfileImage();
-		console.log(user.photoURL)
+		
+		self.setState({
+		user : result.user,
+		userProfileUrl : firebase.auth().currentUser.providerData[0].photoURL
+		})
+		console.log('DisplayName: ', self.state.user)
+			console.log('userProfilePic', self.state.userProfileUrl )
 	});
 };
 
+pushToLocalStorage(){
+	localStorage.setItem("currentUser", "this.state.userProfileUrl");
+}
+/*
 logoutFunction() {
 	firebase.auth().signOut().then(function (result) {
 		console.log("You are no more my friend");
@@ -68,32 +52,27 @@ logoutFunction() {
 		nameHolder.innerHTML = "Somting went wrong";
 	});
 };
-
-displayProfileImage() {
-	imageHolder.innerHTML = "";
-	profileImage = document.createElement("img");
-	profileImage.src = userProfileUrl;
-	profileImage.style.maxHeight = "100%";
-	profileImage.style.maxWidth = "100%";
-	imageHolder.appendChild(profileImage);
-};
 */
 render() {
+	let loginComp;
+	if(this.state.user){ // If user is online
+		loginComp =<p id="nameHolder">Du är nu inloggad</p>;
+		console.log("Inloggad, Gå vidare till sidan")
+		//<button id=logoutButton" onClick={this.logoutFunction}>Logout</button>
+		localStorage.setItem("currentUserPicutre", this.state.userProfileUrl);
+	} else {
+		// User not signed in.
+		console.log("Ej inloggad, Meddelande till användare on inloggning")
+		loginComp = <div><p id="nameHolder">Please login with Facebook aor Google</p><button className="loginBtn loginBtn--facebook" id="loginButton" onClick={this.loginFunction} > Login with Facebook </button><button className="loginBtn loginBtn--google" id="googleLogin" onClick={this.googleFunction}> Login with Google </button> </div>	
+	}
     return (
     <div id="divContaienr">
-		<div id="imageHolder">
-		<p id="imageMessage">No picture</p>
-		</div>
-		<p id="nameHolder">Please login with Facebook aor Google</p>
-		<button className="loginBtn loginBtn--facebook" id="loginButton" onClick={this.loginFunction} > Login with Facebook </button>
-		<button className="loginBtn loginBtn--google" id="googleLogin" onClick={this.googleFunction}> Login with Google </button>
-		<button id="logoutButton" onClick={this.logoutFunction}>Logout</button>
+			{loginComp}
+		
     </div>
         )
     }
 }
-
-
 ReactDOM.render(
         <App/>,
         document.getElementById('loginRoot')
